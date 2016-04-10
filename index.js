49bc2bd40b4988d92c187ee7a31796b44a94972c
Chico <chico.charlesworth@googlemail.com>
2016-04-10 09:02:56 +0100

    Added logging

--- index.js
+++ index.js
@@ -256,6 +256,9 @@ class Bot {
             // if we have text to match and it doesn't, give up
             // if we have a reg ex to match and it doesn't, give up
             // otherwise this is ours }:-)
+            console.dir(incoming);
+            console.log(incoming.type);
+            console.log(incoming.mention);
             if (incoming.isTextMessage()) {
                 if ((!isString && !isRegExp)
                   || (isString && incoming.body === text)) {
